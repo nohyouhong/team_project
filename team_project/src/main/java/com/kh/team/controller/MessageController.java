a6fe7df66998d39487a56439a29e760b65ad5e6c
hoyadobi <rkelgl12@naver.com>
2022-06-03 11:25:00 +0900

    수정된 resources 경로

--- team_project/src/main/java/com/kh/team/controller/MessageController.java
+++ team_project/src/main/java/com/kh/team/controller/MessageController.java
@@ -1,8 +1,15 @@
 package com.kh.team.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/message")
 public class MessageController {
-
+	@RequestMapping(value="/message_list", method = RequestMethod.GET)
+	public String pointList() {
+		
+		return "message/message_list";
+	}
 }

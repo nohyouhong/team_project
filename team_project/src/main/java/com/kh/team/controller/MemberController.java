b5e45c2f5d1f08e8beeb4a9c90a0053143564135
nohyouhong <sino0121@naver.com>
2022-06-03 16:59:17 +0900

    project 진행중

--- team_project/src/main/java/com/kh/team/controller/MemberController.java
+++ team_project/src/main/java/com/kh/team/controller/MemberController.java
@@ -1,13 +1,22 @@
 package com.kh.team.controller;
 
+import java.io.FileInputStream;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kh.team.dao.MemberDao;
 import com.kh.team.service.MemberService;
 import com.kh.team.vo.MemberVo;
 import com.kh.team.vo.PointVo;
@@ -19,7 +28,6 @@ public class MemberController {
 	@Autowired
 	MemberService memberService;
 	
-	
 	@RequestMapping(value="/mapper", method=RequestMethod.GET) 
 	public String home() {
 		return "member/home";
@@ -27,8 +35,8 @@ public class MemberController {
 	
 	@RequestMapping(value="/getMemberById", method=RequestMethod.GET) 
 	public String getMemberById(String userid, Model model) {
-//		MemberVo memberVo = memberService.getMemberById(userid);
-//		model.addAttribute("memberVo", memberVo);
+		MemberVo memberVo = memberService.getMemberById(userid);
+		model.addAttribute("memberVo", memberVo);
 		return "member/info";
 	}
 	
@@ -42,10 +50,61 @@ public class MemberController {
 		return "member/login_form";
 	}
 	
+	@RequestMapping(value="/info", method=RequestMethod.GET)
+	public String info(String userid, Model model) {
+		MemberVo memberVo = memberService.getMemberById(userid);
+		model.addAttribute("memberVo", memberVo);
+		return "member/info";
+	}
+	
 	@RequestMapping(value="/point_list", method = RequestMethod.GET)
 	public String pointList(String userid, Model model) {
 		List<PointVo> point_list = memberService.getPoint_list(userid);
 		model.addAttribute("point_list", point_list);
 		return "point/point_list";
 	};
+	
+	@RequestMapping(value="/login_run", method=RequestMethod.POST)
+	public String login(String userid, String userpw, String saveId, 
+			HttpSession session, RedirectAttributes rttr, HttpServletResponse response) {
+		MemberVo memberVo = memberService.getMemberByIdAndPw(userid, userpw);
+		if(memberVo == null) {
+			rttr.addFlashAttribute("login_result", "fail");
+			return "redirect:/";
+		} else {
+			session.setAttribute("loginVo", memberVo);
+			if(saveId != null && !saveId.equals("")) {
+				Cookie cookie = new Cookie("saveId", userid);
+				cookie.setPath("/");
+				cookie.setMaxAge(60 * 60 * 24 * 7);
+				response.addCookie(cookie);
+			} else {
+				Cookie cookie = new Cookie("saveId", userid);
+				cookie.setPath("/");
+				cookie.setMaxAge(0); 
+				response.addCookie(cookie);
+			}
+			String targetLocation = (String)session.getAttribute("targetLocation");
+			if(targetLocation == null || targetLocation.equals("")) {
+				return "redirect:/";
+			}else {
+				return "redirect:" + targetLocation;
+			}
+		}
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/displayImage", method=RequestMethod.GET)
+	@ResponseBody
+	public byte[] displayImage(String filename) throws Exception{
+		FileInputStream fis = new FileInputStream(filename);
+		byte[] data = IOUtils.toByteArray(fis);
+		fis.close();
+		return data;
+	}
 }
--- team_project/src/main/webapp/WEB-INF/views/member/join_form.jsp
+++ team_project/src/main/webapp/WEB-INF/views/member/join_form.jsp
@@ -1,27 +1,24 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">												
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">												
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>												
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>												
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>												
+
+<%@include file="/WEB-INF/views/include/main_header.jsp" %>
 <style>
 .gender{
 	margin-left: 20px; 
 }
+.window{
+	margin-top:100px;
+	margin-bottom:30px;
+}
+.col-md-12 {
+	text-align: left;
+}
 </style>
-</head>
-<body>
-	<div class="container-fluid">
+	<div class="container-fluid window" >
 		<div class="row">
 			<div class="col-md-12">
-				<form role="form">
+				<form role="form" action="" method="post">
 					<div class="form-group">
 						<label for="userid">아이디</label> 
 						<input type="text" class="form-control" id="userid" name="userid"/>
@@ -64,5 +61,4 @@
 			</div>
 		</div>
 	</div>
-</body>
-</html>
+<%@include file="/WEB-INF/views/include/main_footer.jsp" %>

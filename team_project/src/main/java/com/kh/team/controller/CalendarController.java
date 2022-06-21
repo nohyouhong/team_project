795f19da01ae9810517ce5f6bd1dfc367bba5f95
Authorxzsad <bong890@naver.com>
2022-06-21 16:18:30 +0900

    출석체크시 LoginVo 갱신

--- team_project/src/main/java/com/kh/team/controller/CalendarController.java
+++ team_project/src/main/java/com/kh/team/controller/CalendarController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,9 +19,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.team.service.CalService;
+import com.kh.team.service.MemberService;
 import com.kh.team.vo.DateData;
+import com.kh.team.vo.MemberVo;
 
 
 @Controller
@@ -29,6 +33,9 @@ public class CalendarController {
 	
 	@Autowired
 	private CalService calService;
+	
+	@Autowired
+	private MemberService memberService;
 	
 	@RequestMapping(value = "/chkAttendance", method = RequestMethod.GET)
 	public String calendar(Model model, HttpServletRequest request, DateData dateData, String userid){
@@ -104,18 +111,21 @@ public class CalendarController {
 	}
 	
 	@RequestMapping(value="/insertAttendance", method=RequestMethod.GET)
-	@ResponseBody
-	public String insertAttendance(String userid) {
+		public String insertAttendance(String userid, HttpSession session, RedirectAttributes rttr) {
 		System.out.println("userid: " + userid);
 		Date sysdate = Date.valueOf(LocalDate.now());
 		int count = calService.isAttend(userid, sysdate);
 		if (count > 0) {
-			return "fail";
+			rttr.addFlashAttribute("attend_fail", false);
 		} else {
 			calService.insertAttend(userid);
 			calService.updateMemberTattend(userid);
-			return "success";
+			rttr.addFlashAttribute("attend_success", true);
 		}
+		MemberVo memberVo = (MemberVo)session.getAttribute("loginVo");
+		MemberVo loginVo = memberService.getMemberByIdAndPw(memberVo.getUserid(), memberVo.getUserpw());
+		session.setAttribute("loginVo", loginVo);
+		return "redirect:/cal/chkAttendance?userid=" + userid;
 	}
 	
 }

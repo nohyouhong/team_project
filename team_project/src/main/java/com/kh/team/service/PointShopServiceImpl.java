181d7cebfb96c7b7348b49eba6326cbe5ac1a903
nohyouhong <sino0121@naver.com>
2022-07-01 17:31:42 +0900

    수정

--- team_project/src/main/java/com/kh/team/service/PointShopServiceImpl.java
+++ team_project/src/main/java/com/kh/team/service/PointShopServiceImpl.java
@@ -135,37 +135,37 @@ public class PointShopServiceImpl implements PointShopService{
 			}
 		}
 		
-		//물품등록
-		int pno = pointShopDao.getNextPno();
-		productVo.setPno(pno);
-		pointShopDao.productCreate(pno, p_bno, productVo.getP_name());
-		String[] p_options = productVo.getP_options();
-		int[] p_prices = productVo.getP_prices();
-		int[] p_discounts = productVo.getP_discounts();
-		int[] p_stocks = productVo.getP_stocks();
-		int[] p_sums = productVo.getP_sums();
-		
-		for(int p_sum : p_sums) {
-//					System.out.println(p_sum);
-		}
-		if((p_options != null && p_options.length != 0) &&
-				(p_prices != null && p_prices.length != 0) &&
-				(p_discounts != null && p_discounts.length != 0) &&
-				(p_stocks != null && p_stocks.length != 0) &&
-				(p_sums != null && p_sums.length != 0)) {
-			for(int i = 0; i < p_options.length; i++) {
-				System.out.println(p_sums[i]);
-				ProductVo productInfoVo = new ProductVo(pno, p_options[i], p_prices[i], p_discounts[i], p_stocks[i], p_sums[i]);  
-				pointShopDao.productInfoCreate(productInfoVo);
-			}
-		}
-		String[] pictures2 = productVo.getP_pictures();
-		if(pictures2 != null && pictures2.length != 0) {
-			for(String picture : pictures2) {
-				pointShopDao.productPicCreate(picture, pno);
-			}
-		}
-		
+//		//물품등록
+//		int pno = pointShopDao.getNextPno();
+//		productVo.setPno(pno);
+//		pointShopDao.productCreate(pno, p_bno, productVo.getP_name());
+//		String[] p_options = productVo.getP_options();
+//		int[] p_prices = productVo.getP_prices();
+//		int[] p_discounts = productVo.getP_discounts();
+//		int[] p_stocks = productVo.getP_stocks();
+//		int[] p_sums = productVo.getP_sums();
+//		
+//		for(int p_sum : p_sums) {
+////					System.out.println(p_sum);
+//		}
+//		if((p_options != null && p_options.length != 0) &&
+//				(p_prices != null && p_prices.length != 0) &&
+//				(p_discounts != null && p_discounts.length != 0) &&
+//				(p_stocks != null && p_stocks.length != 0) &&
+//				(p_sums != null && p_sums.length != 0)) {
+//			for(int i = 0; i < p_options.length; i++) {
+//				System.out.println(p_sums[i]);
+//				ProductVo productInfoVo = new ProductVo(pno, p_options[i], p_prices[i], p_discounts[i], p_stocks[i], p_sums[i]);  
+//				pointShopDao.productInfoCreate(productInfoVo);
+//			}
+//		}
+//		String[] pictures2 = productVo.getP_pictures();
+//		if(pictures2 != null && pictures2.length != 0) {
+//			for(String picture : pictures2) {
+//				pointShopDao.productPicCreate(picture, pno);
+//			}
+//		}
+//		
 		
 		return result;
 	}
